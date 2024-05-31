fe90d2bbf0d3b7be303d2db0d2ccec49164f76c9
Pedro Esteves <kropinho211@outlook.com>
2024-05-31 12:02:11 -0300

    Biblioteca do React Router DOM

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from './pages/home/home';
+import CadDespesa from './pages/cad-despesas/cad-despesas';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div>
-  <h1>Java</h1>
-</div>
-  
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/despesa" element={<CadDespesa />} />
+    </Routes>
+  </BrowserRouter>
 );
 
---
+++ src/pages/cad-despesas/cad-despesas.jsx
@@ -0,0 +1,7 @@
+const CadDespesa = () => {
+    return <div>
+        <h1>Cadastro de Despesa</h1>
+    </div>
+}
+
+export default CadDespesa;
