a81e1085e5bb70c51e6d50b6dd15fc08870c165c
Pedro Esteves <kropinho211@outlook.com>
2024-06-03 13:04:33 -0300

    index com pacote de icones

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from './pages/home/home';
 import CadDespesa from './pages/cad-despesas/cad-despesas';
-
+import "./styles/global.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
